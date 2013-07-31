0cfa65fcf84eb0e415fea545c31b7cecebd58e76
Léo Maradan <maradan.leo@gmail.com>
2013-07-31 17:19:44 +0200

    Delete old stuff

--- www/js/jqueryui.js
+++
@@ -1,9 +0,0 @@
-/**********************************
- * PHP Game Framework             *
- * Published under MIT License    *
- * Copyright (c) 2013 Léo Maradan *
- **********************************/
- 
-define(['jquery', 'js!lib/jquery-ui', 'css!lib/jquery-ui-1.10.3.custom.min.css'], function($) {
-
-});
