92ee66a227345abefb04211e3757ac67a7e87b10
Léo Maradan <maradan.leo@gmail.com>
2013-08-02 13:27:04 +0200

    Add Dice Roll
    Add Dice Roll, and authors file

---
+++ api/tools/Dice.php
@@ -0,0 +1,24 @@
+<?php
+
+/**********************************
+ * Dice Roll by Ludovic Magerand  *
+ **********************************/
+
+class Dice {
+
+	const DefaultFaces = 6;
+
+	public static function Roll($nb,$faces = Dice::DefaultFaces) {
+
+		$max = mt_getrandmax()+1.0;
+
+		$u = sqrt(-2*log(mt_rand()/$max));
+		$v = 2*M_PI*mt_rand()/$max;
+
+		$result = round(sqrt(2.0*$nb/3.0)*$u*cos($v)+2.0*$nb);
+
+		return min(max($nb,$result),$faces*$nb);
+
+	}	
+
+}
